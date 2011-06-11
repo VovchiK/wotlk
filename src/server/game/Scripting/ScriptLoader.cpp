9b3bebbf491345d85d6c2982780c0ca7e9270933
den <gid_92@inbox.ru>
2011-06-11 00:59:28 -0700

    add ScriptLoader

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -467,6 +467,7 @@ void AddSC_boss_ick();
 void AddSC_boss_tyrannus();
 void AddSC_instance_halls_of_reflection();   // Halls of Reflection
 void AddSC_halls_of_reflection();
+void AddSC_boss_lich_king_hr();
 void AddSC_boss_falric();
 void AddSC_boss_marwyn();
 void AddSC_boss_lord_marrowgar();       // Icecrown Citadel
@@ -1177,6 +1178,7 @@ void AddNorthrendScripts()
     AddSC_boss_tyrannus();
     AddSC_instance_halls_of_reflection();   // Halls of Reflection
     AddSC_halls_of_reflection();
+    AddSC_boss_lich_king_hr();
     AddSC_boss_falric();
     AddSC_boss_marwyn();
     AddSC_boss_lord_marrowgar();        // Icecrown Citadel
